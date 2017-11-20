d9a14950f91bfd30063ef9f50ed997b44ac2f2d8
wangwenlong <wangwenlong@fcyun.com>
2017-11-20 18:11:23 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,13 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import "./assets/fonts/web-icons/web-icons.css"
+import "./assets/themes/classic/global/css/bootstrap.css"
+import "./assets/themes/classic/base/css/site.css"
+import "./assets/vendor/animsition/animsition.css"
+import "./assets/vendor/formvalidation/formValidation.css"
+import "./assets/vendor/jquery/jquery.min"
+import "./assets/vendor/bootstrap/bootstrap.min.js"
+import "./assets/vendor/formvalidation/formValidation.min.js"
+import "./assets/vendor/formvalidation/framework/bootstrap.min.js"
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+	el: '#app',
+	router,
+	template: '<App/>',
+	components: {
+		App
+	}
 })
